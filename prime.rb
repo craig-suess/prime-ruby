d4012f7bb9f6e13da647493d8c0453b28da49fbb
craig_suess <suesscraig112715@gmail.com>
2021-09-14 06:26:29 -0400

    commit tree c2eb7b20e78e4237bd05c3721b836a1cd77f703b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+# Add  code here
+#def prime?(integer)
+#    return false if integer < 0 || integer == 0 || integer == 1 ;   
+#    (2..integer-1).to_a.all? do |i| 
+#        integer % i != 0 ;
+#    end 
+#end 
+
+def prime?(integer)
+    integer < 0 || integer == 0 || integer == 1 ? false : (2..integer-1).to_a.all? {|i| integer % i != 0} ;
+end 
+   
